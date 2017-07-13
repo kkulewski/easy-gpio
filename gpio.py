e3d24308590a41464136ec89b3359a22e3097964
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-13 21:32:06 +0200

    Add GPIO value SET

--- gpio.py
+++ gpio.py
@@ -8,5 +8,11 @@ def gpioValueRead(number):
 	state = os.popen(cmd).read()
 	return "gpio{} value == {}".format(number, state)
 
+@app.route("/gpio<number>/value/<state>")
+def gpioValueSet(number, state):
+	cmd = 'echo {} > /sys/class/gpio/gpio{}/value'.format(state, number)
+	os.system(cmd)
+	return "gpio{} value set {}".format(number, state)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
