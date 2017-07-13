9998dca7427f6290def2df9fc2142ec53c450805
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-13 21:31:03 +0200

    Add GPIO value check

--- gpio.py
+++ gpio.py
@@ -1,5 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+
+@app.route("/gpio<number>/value")
+def gpioValueRead(number):
+	cmd = 'cat /sys/class/gpio/gpio{}/value'.format(number)
+	state = os.popen(cmd).read()
+	return "gpio{} value == {}".format(number, state)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
